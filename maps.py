157604f08c2974e924cd96b39e21639db9b9a9fb
Sanjay Vijay <27sanjayvijay@gmail.com>
2024-10-26 14:32:56 -0400

    Implement Google Maps API distance calculation

---
+++ maps.py
@@ -0,0 +1,20 @@
+import googlemaps
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gmaps = googlemaps.Client(key=os.getenv('GOOGLE_MAPS_API_KEY'))
+
+def find_distance(origin, destination):
+    # Get distance matrix
+    matrix = gmaps.distance_matrix(origins=origin, destinations=destination)
+
+    # Return distance
+    return matrix['rows'][0]['elements'][0]['distance']['value']
+
+# Test Google Maps API
+if __name__ == '__main__':
+    origin = "400 McCutcheon Dr, West Lafayette, Indiana, 47906"
+    destination = "10 Juniper Hill Rd, Westford, Massachusetts, 01886"
+    print(f"Distance: {find_distance(origin, destination)} meters")
