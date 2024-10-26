7393b08ca4f90a9f9fffb7e5953480eff4ec7ac3
Sanjay Vijay <27sanjayvijay@gmail.com>
2024-10-26 17:43:12 -0400

    Update map on all pages

--- app.py
+++ app.py
@@ -7,10 +7,34 @@ load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    api_key = os.getenv('GOOGLE_MAPS_API_KEY')
+    api_url = f"https://maps.googleapis.com/maps/api/js?key={api_key}&callback=initMap"
+    return render_template('index.html', api_url=api_url)
+
+@app.route('/about.html')
+def about():
+    api_key = os.getenv('GOOGLE_MAPS_API_KEY')
+    api_url = f"https://maps.googleapis.com/maps/api/js?key={api_key}&callback=initMap"
+    return render_template('about.html', api_url=api_url)
+
+@app.route('/createAccount.html')
+def createAccount():
+    api_key = os.getenv('GOOGLE_MAPS_API_KEY')
+    api_url = f"https://maps.googleapis.com/maps/api/js?key={api_key}&callback=initMap"
+    return render_template('createAccount.html', api_url=api_url)
+
+@app.route('/home.html')
 def home():
     api_key = os.getenv('GOOGLE_MAPS_API_KEY')
     api_url = f"https://maps.googleapis.com/maps/api/js?key={api_key}&callback=initMap"
     return render_template('home.html', api_url=api_url)
+
+@app.route('/map.html')
+def map():
+    api_key = os.getenv('GOOGLE_MAPS_API_KEY')
+    api_url = f"https://maps.googleapis.com/maps/api/js?key={api_key}&callback=initMap"
+    return render_template('map.html', api_url=api_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
